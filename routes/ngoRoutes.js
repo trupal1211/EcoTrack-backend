4abe9f61373abf55ed8983523930ebfd5a7fef10
Trupal Godhat <136154999+trupal1211@users.noreply.github.com>
2025-07-04 23:19:29 +0530

    Update ngoRoutes.js

--- routes/ngoRoutes.js
+++ routes/ngoRoutes.js
@@ -13,12 +13,11 @@ const role = require('../middleware/roleMiddleware')
 const upload = require('../middleware/upload')
 
 router.use(auth);
-router.use(role(['ngo','admin']));
 
-router.put("/take/:reportId", takeReport);
-router.put("/complete/:reportId", upload.array("resolvedImages", 5), completeReport);
-router.get("/taken", getTakenReports);
-router.get("/completed",  getCompletedReports);
-router.get("/uncompleted", getUncompletedReports);
+router.put("/take/:reportId", role(['ngo','admin']), takeReport);
+router.put("/complete/:reportId", role(['ngo','admin']), upload.array("resolvedImages", 5), completeReport);
+router.get("/taken/:userId", getTakenReports);
+router.get("/completed/:userId",  getCompletedReports);
+router.get("/uncompleted/:userId", getUncompletedReports);
 
 module.exports = router;
