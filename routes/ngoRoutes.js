ea27d390548a585babc77088c8bbf42ad249391b
Trupal Godhat <136154999+trupal1211@users.noreply.github.com>
2025-07-06 10:54:42 +0530

    spell change

--- routes/ngoRoutes.js
+++ routes/ngoRoutes.js
@@ -18,6 +18,6 @@ router.put("/take/:reportId", role(['ngo','admin']), takeReport);
 router.put("/complete/:reportId", role(['ngo','admin']), upload.array("resolvedImages", 5), completeReport);
 router.get("/taken/:userId", getTakenReports);
 router.get("/completed/:userId",  getCompletedReports);
-router.get("/uncompleted/:userId", getUncompletedReports);
+router.get("/incompleted/:userId", getUncompletedReports);
 
 module.exports = router;
